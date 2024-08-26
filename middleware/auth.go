2040f47b3ce9549825036077132026c79fb0e2f5
Radoje <megasrbin1@gmail.com>
2024-08-26 17:19:55 +0200

    remove logs

--- middleware/auth.go
+++ middleware/auth.go
@@ -1,13 +1,11 @@
 package middleware
 
 import (
-	"fmt"
 	"net/http"
 )
 
 func Auth(h http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Println("Hi mom!")
 		h.ServeHTTP(w, r)
 	})
 }
